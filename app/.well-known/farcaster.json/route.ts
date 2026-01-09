c8dab25ca84ca8bb11b481af7a254a18dd05903f
Yhonatan Peguero <yhonatanpm01@gmail.com>
2026-01-08 22:02:08 -0400

    Initial commit

---
+++ app/.well-known/farcaster.json/route.ts
@@ -0,0 +1,8 @@
+import { withValidManifest } from "@coinbase/onchainkit/minikit";
+import { minikitConfig } from "../../../minikit.config";
+
+export async function GET() {
+  return Response.json(withValidManifest(minikitConfig));
+}
+
+const test = "hola";
